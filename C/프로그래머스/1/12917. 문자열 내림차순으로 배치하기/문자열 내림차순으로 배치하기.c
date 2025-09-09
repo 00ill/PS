9b005076fb3eefd964d32beb04955501bb7555eb
00ill <101080818+00ill@users.noreply.github.com>
2025-09-09 22:58:50 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 4.45 MB -BaekjoonHub

---
+++ C/프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+int compare_char(const void* a, const void* b)
+{
+    char arg1 = *(const char*)a;
+    char arg2 = *(const char*)b;
+    return (arg1 < arg2) - (arg2 < arg1);
+}
+
+char* solution(const char* s) {
+    
+    char* answer = (char*)malloc(sizeof(char) * (strlen(s) + 1));
+    strcpy(answer, s);
+    qsort(answer, strlen(answer), sizeof(char), compare_char);
+    return answer;
+}
