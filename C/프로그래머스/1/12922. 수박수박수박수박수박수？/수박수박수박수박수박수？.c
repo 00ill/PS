938f61fbe1d80bee46dff18bbdcf41ef5a76c662
00ill <101080818+00ill@users.noreply.github.com>
2025-09-09 22:45:13 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.90 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ C/프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+char* solution(int n) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+    char* answer = (char*)malloc((sizeof(char) * n * 4) + 1);
+    strcpy(answer, "");
+    for (int i = 0; i < n; i++)
+    {
+        if(i & 1)
+        {
+            strcat(answer, "박");
+        }
+        else
+        {
+            strcat(answer, "수");
+        }
+    }
+    return answer;
+}
