5bfbed6d0207125412334833ac6c7ea240c5d9f7
00ill <101080818+00ill@users.noreply.github.com>
2025-09-12 17:23:09 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.36 ms, Memory: 5.67 MB -BaekjoonHub

--- C/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.c
+++ C/프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.c
@@ -7,7 +7,6 @@ int* solution(const char* s) {
     int answer_length = strlen(s);
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(sizeof(int) * answer_length);
-    int index = 0;
     int record[26] = {0};
     
     for (int i = 0; i < 26; i++) record[i] = -1;
