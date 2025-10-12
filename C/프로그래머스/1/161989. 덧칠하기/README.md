b40ee10952d419bb41f9c468e8769f1c10be0209
00ill <101080818+00ill@users.noreply.github.com>
2025-10-13 08:35:09 +0900

    [level 1] Title: 덧칠하기, Time: 24.60 ms, Memory: 4.99 MB -BaekjoonHub

--- C/프로그래머스/1/161989. 덧칠하기/README.md
+++ C/프로그래머스/1/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989#qna) 
 
 ### 성능 요약
 
-메모리: 4.66 MB, 시간: 0.15 ms
+메모리: 4.99 MB, 시간: 24.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 10일 16:09:08
+2025년 10월 13일 08:35:07
 
 ### 문제 설명
 
--- C/프로그래머스/1/161989. 덧칠하기/덧칠하기.c
+++ C/프로그래머스/1/161989. 덧칠하기/덧칠하기.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
 // section_len은 배열 section의 길이입니다.
 int solution(int n, int m, int section[], size_t section_len) {
-
     int answer = 0;
     int index = section[0];
-    int i = 0;
-
-    while (i < section_len) 
+    if(m == 1) return section_len;
+    while(index <= section[section_len - 1])
     {
-        index = section[i] + m - 1;
+        index += m;
         answer++;
-        while (i < section_len && section[i] <= index) i++;
+        for(int i = 0; i < section_len; i++)
+        {
+            if (index <=section[i])
+            {
+                index = section[i];
+                break;
+            }
+        }
     }
-
     return answer;
-
 }
+
