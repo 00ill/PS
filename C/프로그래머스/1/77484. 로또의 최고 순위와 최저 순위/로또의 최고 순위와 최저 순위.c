5b8d10bf9f07fd261fae51aef35b1fa80edef226
00ill <101080818+00ill@users.noreply.github.com>
2025-10-13 14:29:57 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ C/프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// lottos_len은 배열 lottos의 길이입니다.
+// win_nums_len은 배열 win_nums의 길이입니다.
+int* solution(int lottos[], size_t lottos_len, int win_nums[], size_t win_nums_len) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int) * 2);
+    int rank[7] = {6, 6, 5, 4, 3, 2, 1};
+    int same = 0;
+    int zero = 0;
+    for(int i = 0; i < 6; i++)
+    {
+        if(lottos[i] == 0 ) zero++;
+        else
+        {
+            for(int j = 0; j < 6; j++)
+            {
+                if(lottos[i] == win_nums[j]) same++;
+            }
+        }
+            
+    }
+    answer[1] = rank[same];
+    
+    for(int i = zero; i >=0; i++)
+    {
+        if(same + i <= 6) { answer[0] = rank[same +i]; break;}
+    }
+    
+    return answer;
+}
