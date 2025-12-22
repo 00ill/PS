269706878432f78e065c15f196adc5c5ce65a855
00ill <101080818+00ill@users.noreply.github.com>
2025-12-22 14:54:41 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ C/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+bool can_pron(const char* pron[], char* bab, int len[])
+{
+    if(bab[0] == '\0') return true;
+    char *p = bab;
+    for(int i = 0; i < 4; i++)
+    {
+        if(strncmp(pron[i], p, len[i]) == 0)
+        {
+            p = p + len[i];
+            return can_pron(pron, p, len);
+        }
+    }    
+    return false;    
+}
+
+int solution(const char* babbling[], size_t babbling_len) {
+    int answer = 0;
+    const char* pron[4] = {"aya", "ye", "woo", "ma"};
+    const int len[4] = {3, 2, 3, 2};
+    for(int i = 0; i < babbling_len; i++)
+    {
+        if(can_pron(pron, babbling[i], len) == true) answer++;
+    }
+    return answer;
+}
