8ecdcdcdc9acf22a8494e81fa6d43d85de84c97b
00ill <101080818+00ill@users.noreply.github.com>
2026-01-05 14:35:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- C/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.c
+++ C/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+
 char* solution(const char* s) {
-    int length = strlen(s);
-    char* answer = (char*)malloc(sizeof(char) * 3);
-    
-    if(length % 2 == 0)
+    size_t len = strlen(s);
+    char* answer = calloc(3, 1);
+    if(len & 1)
     {
-        answer[0] = s[length/2 - 1];
-        answer[1] = s[length/2];
-        answer[2] = '\0';
+        answer[0] = s[(len - 1) / 2];
+        
     }
     else
     {
-        answer[0] = s[length/2];
-        answer[1] = '\0';
+        answer[0] = s[len / 2 - 1];
+        answer[1] = s[len / 2];
+        answer[2] = '\0';
     }
-    
     return answer;
 }
