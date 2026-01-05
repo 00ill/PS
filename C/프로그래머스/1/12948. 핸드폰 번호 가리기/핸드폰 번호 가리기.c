2856d1d020fd1453637f3383617b6127cd8dfa93
00ill <101080818+00ill@users.noreply.github.com>
2026-01-05 14:32:08 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- C/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.c
+++ C/프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+
 char* solution(const char* phone_number) {
-    int length = strlen(phone_number);
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    char* answer = (char*)malloc(sizeof(char) * (length + 1));
-    for(int i = 0; i < length; i++)    
+    size_t len = strlen(phone_number);
+    char* answer = calloc(len + 1, 1);
+    strcpy(answer, phone_number);
+    for(int i = 0; i < len - 4; i++)
     {
-        if(i < length - 4)
-        {
-            answer[i] = '*';
-        }
-        else
-        {
-            answer[i] = phone_number[i];
-        }
+        answer[i] = '*';
     }
-    answer[length] = '\0';
     return answer;
 }
