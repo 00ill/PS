3394fd2c414fdfdf3008e560e03cdc5eb5aeb4ab
00ill <101080818+00ill@users.noreply.github.com>
2026-01-07 09:10:33 +0900

    [level 0] Title: 다항식 더하기, Time: 0.03 ms, Memory: 4 MB -BaekjoonHub

--- C/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.c
+++ C/프로그래머스/0/120863. 다항식 더하기/다항식 더하기.c
@@ -2,9 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 char* solution(const char* polynomial) {
-    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     char* answer = (char*)malloc(512);
     int len = strlen(polynomial);
     char *str = calloc(len + 1, 1);
@@ -16,7 +14,7 @@ char* solution(const char* polynomial) {
         int token_len = strlen(token);
         int temp;
         if(token[0] == '+') {}
-        else if(token[0] == 'x') {printf("x\n"); x++;}
+        else if(token[0] == 'x') x++;
         else if(token[token_len - 1] == 'x')
         {
             sscanf(token, "%dx ", &temp);
@@ -30,8 +28,16 @@ char* solution(const char* polynomial) {
         token = strtok(NULL, " ");
         if(token == NULL) break;
     }
-    if(num == 0) {sprintf(answer, "%dx", x); if(x == 1) {sprintf(answer, "%s", "x");}}
-    else if(x == 0) {sprintf(answer, "%d", num);}
-    else {sprintf(answer, "%dx + %d", x, num); if(x == 1) {sprintf(answer, "%s + %d", "x", num);}}
+    if(num == 0) 
+    {
+        if(x == 1) sprintf(answer, "%s", "x");
+        else sprintf(answer, "%dx", x); 
+    }
+    else if(x == 0) sprintf(answer, "%d", num);
+    else 
+    {
+        if(x == 1) {sprintf(answer, "%s + %d", "x", num);}
+        else sprintf(answer, "%dx + %d", x, num); 
+    }
     return answer;
 }
