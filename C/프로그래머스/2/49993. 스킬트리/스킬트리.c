f0279aa30846442362101d517d8f9bdc6d890c2b
00ill <101080818+00ill@users.noreply.github.com>
2026-01-08 14:16:48 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ C/프로그래머스/2/49993. 스킬트리/스킬트리.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+// skill_trees_len은 배열 skill_trees의 길이입니다.
+int solution(const char* skill, const char* skill_trees[], size_t skill_trees_len) {
+    int answer = 0, target_index = 0;
+    for(int i = 0; i < skill_trees_len; i++)
+    {
+        char *current = skill_trees[i];
+        printf("%s 스킬트리 검사\n", current);
+        while(*current)
+        {
+            char temp[2];
+            temp[0] = *current;
+            temp[1] = '\0';
+            if(strstr(skill, temp) != NULL)
+            {
+                printf("%c는 스킬트리에 있음\n", *current);
+                if(skill[target_index] == *current) {target_index++; printf("%c 를 배울 차례였다\n", *current);}
+                else 
+                {
+                    printf("%c를 배울 차례가 아니였다\n", *current);
+                    break;
+                }
+            }
+            current++;
+            if(*current == NULL) {answer++;printf("%s는 이상이 없다\n", skill_trees[i]);}
+        }
+        target_index = 0;
+    }
+    return answer;
+}
